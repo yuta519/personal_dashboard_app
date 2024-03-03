7615dce8b95cabc440138d31c9aa47619a9f6138
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-03-02 23:50:00 -0800

    Apply Header component

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Container from "../components/Container";
 import useUsers from "../hooks/useUsers";
+import Header from "../components/Header";
 
 const Root = () => {
   const { user } = useUsers();
   if (!user.email || !user.name) window.location.href = "/signup";
 
   return (
-    <Container>
-      <Outlet />
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <Outlet />
+      </Container>
+    </>
   );
 };
 
