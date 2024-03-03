2cc015fe8be5ac0fbd01e1e49e5d2b1c51e7beef
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-03-02 23:09:41 -0800

    Create function to fetch weather

--- src/Infra/OpenWeather.ts
+++ src/Infra/OpenWeather.ts
@@ -1,5 +1,48 @@
 import axios, { AxiosError } from "axios";
 import { City } from "../types/City";
+import { Weather } from "../types/Weather";
+
+type WeatherResponse = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
 
 export class OpenWeather {
   private apiKey: string;
@@ -10,14 +53,9 @@ export class OpenWeather {
     this.url = "http://api.openweathermap.org";
   }
 
-  public async getCityByName(
-    city: string,
-    country: string = "",
-    limit: number = 10
-  ) {
+  private async getRequest<T>(url: string): Promise<T> {
     try {
-      const url = `${this.url}/geo/1.0/direct?q=${city},${country}&limit=${limit}&appid=${this.apiKey}`;
-      const result: City[] = await axios.get(url).then((res) => res.data);
+      const result = await axios.get(url).then((res) => res.data);
       return result;
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -31,5 +69,35 @@ export class OpenWeather {
         "Something went wrong. Please ask service administrator."
       );
     }
+  }
+
+  public async getCityByName(
+    city: string,
+    country: string = "",
+    limit: number = 10
+  ) {
+    const url = `${this.url}/geo/1.0/direct?q=${city},${country}&limit=${limit}&appid=${this.apiKey}`;
+    return await this.getRequest<City[]>(url);
+  }
+
+  public async getWeatherByLatAndLon(
+    lat: number,
+    lon: number
+  ): Promise<Weather> {
+    const url = `${this.url}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
+    const response = await this.getRequest<WeatherResponse>(url);
+    const weather: Weather = {
+      name: response.name,
+      weather: response.weather,
+      temperature: {
+        avg: response.main.temp,
+        feelsLike: response.main.feels_like,
+        min: response.main.temp_min,
+        max: response.main.temp_max,
+      },
+      pressure: response.main.pressure,
+      humidity: response.main.humidity,
+    };
+    return weather;
   }
 }
