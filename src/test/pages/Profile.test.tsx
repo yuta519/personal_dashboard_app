4b9c22af76d094fed185fb5aee0d1d1d74564cd2
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2024-03-04 16:02:11 -0800

    Add test for Profile page

--- src/test/pages/Profile.test.tsx
+++ src/test/pages/Profile.test.tsx
@@ -1,31 +1,42 @@
-import React from "react";
-// import { render, fireEvent, screen } from "@testing-library/react";
-// import Profile from "../../pages/Profile";
+import { render, screen } from "@testing-library/react";
+import Profile from "../../pages/Profile";
+import useUsers from "../../hooks/useUsers";
 
-test("renders title", () => {
-  //   const useStateSpy = jest.spyOn(React, "useUsers");
-  //   const useStateMock = useStateSpy.mockImplementation((init) => [
-  //     init,
-  //     jest.fn(),
-  //   ]);
-  //   render(<Profile />);
-  //   const linkElement = screen.getByText(/learn react/i);
-  //   expect(linkElement).toBeInTheDocument();
+jest.mock("../../hooks/useUsers");
+const mockUseUsers = jest.mocked(useUsers);
+
+const mockUser = {
+  name: "John Doe",
+  email: "john@example.com",
+  avatar: "https://example.com/avatar.png",
+};
+
+describe("Profile", () => {
+  it("render profile page title", () => {
+    mockUseUsers.mockImplementation(() => ({
+      user: mockUser,
+      signup: jest.fn(),
+    }));
+    render(<Profile />);
+
+    const titleElement = screen.getByText(/Profile/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it("render mocked user information", () => {
+    mockUseUsers.mockImplementation(() => ({
+      user: mockUser,
+      signup: jest.fn(),
+    }));
+    render(<Profile />);
+
+    const nameElement = screen.getByText(mockUser.name);
+    expect(nameElement).toBeInTheDocument();
+
+    const emailElement = screen.getByText(mockUser.email);
+    expect(emailElement).toBeInTheDocument();
+
+    const avatarElement = screen.getByAltText(mockUser.name);
+    expect(avatarElement).toBeVisible();
+  });
 });
-
-// test("renders the component and increments the count", () => {
-//   const useStateSpy = jest.spyOn(React, "useState");
-//   const useStateMock = useStateSpy.mockImplementation((init) => [
-//     init,
-//     jest.fn(),
-//   ]);
-
-//   const { getByText } = render(<MyComponent />);
-
-//   const buttonElement = getByText(/increment/i);
-//   expect(buttonElement).toBeInTheDocument();
-
-//   fireEvent.click(buttonElement);
-
-//   useStateMock.mockRestore();
-// });
